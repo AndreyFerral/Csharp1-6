db77c6b5967dd17dbbc6cd8d089880aaaeb1cc6d
Andrey Yakovlev <tigra9922@gmail.com>
2020-05-06 18:16:20 +1000

    Добавьте файлы проекта.

---
+++ lab1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x;
+            double a, b;
+            Console.WriteLine("Введите x");
+            double.TryParse(Console.ReadLine(), out x);
+            a = (Math.Sqrt(Math.Pow(3 * x + 2, 2) - 24 * x)) / (3 * Math.Sqrt(x) - (2 / Math.Sqrt(x)));
+            b = -(Math.Sqrt(x));
+            Console.WriteLine("Первая переменная: " + a);
+            Console.WriteLine("Вторая переменная: " + b);
+            Console.ReadKey();
+        }
+    }
+}
